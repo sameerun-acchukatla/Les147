6cc5e365cd05c6c0d793fefe555c8756b4433d7f
unknown <sameerun.shaikh@gmail.com>
2022-01-14 13:51:49 +0100

    Ist version of my Calculator

---
+++ src/main/java/com/example/les147/Calculator.java
@@ -0,0 +1,23 @@
+package com.example.les147;
+
+public class Calculator {
+
+    public static void main(String[] args)
+
+    {
+        System.out.println(sum(-25, 50));
+
+        System.out.println(mul(5,4));
+    }
+
+    public static double sum(double n1, double n2){
+
+        return Math.abs(n1) + Math.abs(n2);
+
+    }
+    public static double mul(double n1, double n2){
+
+        return Math.abs(n1) * Math.abs(n2);
+
+    }
+}
